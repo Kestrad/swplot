8e4b47852ac30f2fa755330e7fe96d4cd8773e31
Kestrad <firedreamer@gmail.com>
2021-08-07 02:16:21 -0700

    Add api call to SWAPI and ability to charts from that data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Plot from 'react-plotly.js';
 
 function App() {
+  const [hasData, setHasData] = useState(false);
+  const [planetaryData, setPlanetaryData] = useState([]);
+  useEffect(() => {
+    let data = [];
+    function grabPlanets(url) {
+      return fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          data.push(...res.results);
+          if (res.next) {
+            return grabPlanets(res.next);
+          }
+        })
+    }
+    grabPlanets(`https://swapi.dev/api/planets`)
+      .then(() => {
+        data = data.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        })
+        setPlanetaryData(data);
+        setHasData(true);
+      });
+  }, []);
+
+  const [planetsPage, setPlanetsPage] = useState(1);
+  
+  function getPlanetNames(page, size=10) {
+    let startIndex = (page-1)*10;
+    let names = [];
+    for (let i=startIndex; i<startIndex+size; i++) {
+      names.push(planetaryData[i].name);
+    }
+    return names;
+  }
+
+  function getPlanetPops(page, size=10) {
+    let startIndex = (page-1)*10;
+    let pops = [];
+    for (let i=startIndex; i<startIndex+size; i++) {
+      pops.push(planetaryData[i].population);
+    }
+    return pops;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Star Wars Planetary Charts</h1>
       </header>
+      <div className="Charts">
+        {hasData ? <Plot
+          data={
+            [{type: 'bar', x: getPlanetNames(planetsPage), y: getPlanetPops(planetsPage)},]
+          }
+          layout={{title: "Planet Populations"}}
+          /> : "Loading..."}
+      </div>
     </div>
   );
 }
